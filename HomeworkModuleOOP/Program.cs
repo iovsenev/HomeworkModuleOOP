8d26db4a7edf1e50647ac39922a1d23234ef6663
Ilya Ovsenev <ovsenev.ilya@yandex.ru>
2023-12-09 21:54:53 +0300

    Добавьте файлы проекта.

---
+++ HomeworkModuleOOP/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace HomeworkModuleOOP
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
